df1eeac362a607f9537ec81048150a3fa0fbedd5
Drina <drinabaptiste@gmail.com>
2023-11-15 10:43:43 -0500

    Sign in complete

--- client/react-capstone/src/Components/ButtonAppBar.js
+++ client/react-capstone/src/Components/ButtonAppBar.js
@@ -5,9 +5,22 @@ import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
 import MenuIcon from '@mui/icons-material/Menu';
+import { Link } from 'react-router-dom'; // Assuming you're using React Router
 
 export default function ButtonAppBar() {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
@@ -18,9 +31,36 @@ export default function ButtonAppBar() {
             color="inherit"
             aria-label="menu"
             sx={{ mr: 2 }}
+            onClick={handleClick}
           >
             <MenuIcon />
           </IconButton>
+          <Menu
+            id="menu-appbar"
+            anchorEl={anchorEl}
+            anchorOrigin={{
+              vertical: 'top',
+              horizontal: 'right',
+            }}
+            keepMounted
+            transformOrigin={{
+              vertical: 'top',
+              horizontal: 'right',
+            }}
+            open={Boolean(anchorEl)}
+            onClose={handleClose}
+          >
+            <MenuItem onClick={handleClose}>
+              <Link to="/personal" style={{ textDecoration: 'none', color: 'inherit' }}>
+                Personal Objectives
+              </Link>
+            </MenuItem>
+            <MenuItem onClick={handleClose}>
+              <Link to="/organization" style={{ textDecoration: 'none', color: 'inherit' }}>
+                Organization Objectives
+              </Link>
+            </MenuItem>
+          </Menu>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             OKR Tracker
           </Typography>
@@ -30,6 +70,7 @@ export default function ButtonAppBar() {
     </Box>
   );
 }
+
 
 {/* <Link color="inherit" href="https://mui.com/">
         OKR Tracker
