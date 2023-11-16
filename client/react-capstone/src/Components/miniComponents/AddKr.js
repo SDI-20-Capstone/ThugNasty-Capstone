9ff8bd941ef7a07023a65db3e93ef99ea51fa50a
Drina <drinabaptiste@gmail.com>
2023-11-16 17:03:43 -0500

    add kr done

---
+++ client/react-capstone/src/Components/miniComponents/AddKr.js
@@ -0,0 +1,108 @@
+import React from 'react';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import dayjs from 'dayjs';
+import { DateRangePicker } from '@mui/x-date-pickers-pro/DateRangePicker';
+import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
+import { DemoItem } from '@mui/x-date-pickers/internals/demo';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { useState } from 'react';
+import Toggle from './Toggle';
+
+
+export default function AddKr() {
+  const [open, setOpen] = useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const [value, setValue] = React.useState([
+    dayjs('2022-04-17'),
+    dayjs('2022-04-21'),
+  ]);
+
+  return (
+    <div>
+      <Button variant="outlined" color="primary" onClick={handleClickOpen}>
+        Add KR
+      </Button>
+      <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
+        <DialogTitle id="form-dialog-title">Key Result</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            Organization: Org will be here
+          </DialogContentText>
+          
+        <TextField
+          autoFocus
+          margin='dense'
+          id='name'
+          label='Title'
+          type='Title'
+          fullWidth
+          />
+          <TextField
+            autoFocus
+            margin="dense"
+            id="name"
+            label="Mission Impact"
+            type="Mission"
+            fullWidth
+          />
+          <div>
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <DemoContainer components={['DateRangePicker', 'DateRangePicker']}>
+        <DemoItem  component="DateRangePicker">
+          <DateRangePicker
+            value={value}
+            onChange={(newValue) => setValue(newValue)}
+          />
+        </DemoItem>
+      </DemoContainer>
+    </LocalizationProvider>
+    <div style={{display: 'flex', justifyContent: 'center'}}>
+      target_percent  <Toggle/> target_value
+    </div>
+        </div>
+        
+          <TextField
+          autoFocus
+          margin='dense'
+          id='name'
+          label='Percent Input'
+          type='number'
+          fullWidth
+          />
+          Or
+          <TextField
+          autoFocus
+          margin='dense'
+          id='name'
+          label='Value Input'
+          type='number'
+          fullWidth
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="primary">
+            Cancel
+          </Button>
+          <Button onClick={handleClose} color="primary">
+            Submit
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+}
