7016ad1076084424217f114f410dc3cb174cf35d
Louis <lst9829@yahoo.com>
2023-11-30 12:35:22 -0700

    fixed signup issues

--- server/Helpers.js
+++ server/Helpers.js
@@ -1,6 +1,20 @@
 const knex = require("knex")(require("./knexfile.js")["development"]);
 
-const createNewUser = (first_name,last_name,email,password,organization_id,rank,) => {
+const createNewUserSignUp = (first_name,last_name,email,password,organization,rank) => {
+  const orgId = knex('organization').select('organization.id').where("name", "=", organization)
+
+  return knex("userinfo").insert([{
+    first_name: first_name,
+    last_name: last_name,
+    email: email,
+    password: password,
+    organization_id: orgId,
+    rank: rank,
+    role: "user"
+  }])
+}
+
+const createNewUserAdmin = (first_name, last_name, email, password, organization_id, rank) => {
   const orgId = knex('organization').select('organization.id').where("id", "=", organization_id)
 
   return knex("userinfo").insert([{
@@ -73,4 +87,4 @@ const createNewPersMeasurement = (personal_key_result_id, date, count, success, 
 }
 
 
-module.exports = {createNewUser, updateMember, createNewMeasurement, patchKeyResult, persPatchKeyResult, createNewPersMeasurement}
+module.exports = {createNewUserSignUp, createNewUserAdmin, updateMember, createNewMeasurement, patchKeyResult, persPatchKeyResult, createNewPersMeasurement}
